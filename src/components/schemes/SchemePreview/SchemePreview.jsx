430514038f68189abf9a7149d1db91cfff6be43d
Vova Rodin <mynameisvova@yahoo.com>
2022-02-03 15:21:12 +0300

    Устанавливает обязательный аттр key в комп SchemesPreview

--- src/components/schemes/SchemePreview/SchemePreview.jsx
+++ src/components/schemes/SchemePreview/SchemePreview.jsx
@@ -1,19 +1,21 @@
 import scss from './SchemePreview.module.scss'
 
 import classNames from 'classnames'
+import { uid } from 'uid'
 
 import Row from '@components/canvas/Row/Row'
 import CanvasCell from '@components/canvas/CanvasCell/CanvasCell'
 
-export default function Empty ({ className, canvas }) {
+export default function SchemePreview ({ className, canvas }) {
 
   return (
     <div className={classNames(className, scss._)}>
       <div className={scss.canvas}>
-        {canvas.map((row, index) => (
-          <Row key={index}>
+        {canvas.map(row => (
+          <Row key={uid()}>
             {row.map(cell => (
               <CanvasCell
+                key={uid()}
                 className={scss.cell}
                 isPreview={true}
                 cell={cell} />
